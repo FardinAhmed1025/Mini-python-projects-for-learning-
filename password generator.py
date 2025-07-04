14d6a3d67efe9d17e40b00718498e49c2ca2236c
Fardin Ahmed <153766474+FardinAhmed1025@users.noreply.github.com>
2025-07-05 03:24:26 +0600

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -5,8 +5,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to PyPass!")
 nr_letters = int(input("How many letters would you like in your pass?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like your password to contain?\n"))
+nr_numbers = int(input(f"How many numbers would you like would you like your password to contain?\n"))
 
 password_list = []
 for char in range(0, nr_letters):
